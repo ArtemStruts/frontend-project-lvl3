9a964ee05a37e118e003023b9ab227179de1ad29
Artem Struts <strutsart7@gmail.com>
2021-10-31 21:47:48 +0300

    test2

--- src/init.js
+++ src/init.js
@@ -80,7 +80,7 @@ const app = () => {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
       const formData = new FormData(e.target);
-      const value = formData.get('url');
+      const value = formData.get('url').trim();
       validator(schema, value, watchedState, i18nextInstance);
       axios.get(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${encodeURIComponent(value)}`)
         .then((response) => {
